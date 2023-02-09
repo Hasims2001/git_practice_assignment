850e25d94d6c2bc4df05d2bae7233a3e8718aab7
Hasims2001 <hasims2001@gmail.com>
2023-02-09 12:07:02 +0530

    prime and palindrome

---
+++ prime.js
@@ -0,0 +1,13 @@
+// Problem 1 : Check whether a number is Prime or not
+let check = 12;
+let counter = 0;
+for(let i=1; i<check; i++){
+    if(check%i == 0){
+        counter++;
+    }
+}
+// if(counter == 2){
+//     console.log("Prime Number");
+// }else{
+//     console.log("Not Prime Number");
+// }
